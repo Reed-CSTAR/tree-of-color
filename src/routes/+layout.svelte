715ed3bd1c60a06b445b9012281a8387c80387b5
Tristan F. <tristanf@reed.edu>
2025-04-05 16:18:42 -0700

    feat: vim button

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import '@fontsource/fira-sans';
+    import './app.css'
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import Monaco from 'svelte-monaco/Monaco.svelte';
+	import type MonacoE from 'monaco-editor';
 	import examplepy from '../../py/example.py?raw';
 	import PyodideWorker from '../lib/worker?worker';
 	import Lights from '$lib/Lights.svelte';
+	import { onMount } from 'svelte';
+	import Icon from '@iconify/svelte';
+	import tree from '@iconify-icons/iconoir/tree';
+	import share from '@iconify-icons/iconoir/share-android';
+	import vim from '@iconify-icons/vscode-icons/file-type-vim';
 
 	/** The current script */
 	let value = $state<string>(examplepy);
@@ -18,6 +24,21 @@
 	let lights = $state<Uint8Array>();
 
     let lastFrameTime: number;
+
+	let editor = $state<MonacoE.editor.IStandaloneCodeEditor>()
+	let vimMode = $state(false)
+
+	let initVimMode: any;
+	let vimInstance: any;
+
+	onMount(async () => {
+		const mvImport = await import('monaco-vim');
+		initVimMode = mvImport.initVimMode;
+
+		value = window.localStorage.getItem("toc:content") ?? ""
+	});
+
+	$effect(() => window.localStorage.setItem("toc:content", value))
 
 	async function run() {
 		worker?.terminate();
@@ -48,6 +69,17 @@
 		cancelAnimationFrame(raf);
 	}
 
+	$effect(() => {
+		console.log(editor)
+		if (editor) {
+			if (vimMode) {
+				vimInstance = initVimMode(editor, document.getElementById('statusBar'))
+			} else {
+				vimInstance?.dispose();
+			}
+		}
+	})
+
 	function frame() {
         if (Date.now() - lastFrameTime > 1000) {
             Atomics.notify(new Int32Array(buffer), 0);
@@ -59,14 +91,38 @@
 </script>
 
 <div class="container">
-	<header></header>
+	<header>
+		<div class="left">
+			<div class="icon">
+				<Icon icon={tree} color="white" width="100%" height="100%" />
+			</div>
+			<h1>Tree of Color</h1>
+		</div>
+		<div class="right">
+			<div class="iconAlign icon">
+				<button class="smallIcon">
+					<Icon icon={share} color="white" width="100%" height="100%" />
+				</button>
+			</div>
+			<div class="iconAlign icon">
+				<button class="smallIcon" onclick={() => vimMode = !vimMode}>
+					<Icon icon={vim} color="white" width="100%" height="100%" />
+				</button>
+			</div>
+		</div>
+	</header>
 	<main>
-		<Monaco
-			options={{ language: 'python', automaticLayout: true }}
-			theme="vs-dark"
-			on:ready={(event) => console.log(event.detail)}
-			bind:value
-		/>
+		<div class="editor">
+			<Monaco
+				options={{ language: 'python', automaticLayout: true }}
+				theme="vs-dark"
+				on:ready={async ({ detail }) => {
+					editor = detail;
+				}}
+				bind:value
+			/>
+			<div id="statusBar" class:vimMode></div>
+		</div>
 		<div class="vis">
 			<button onclick={run}>Run</button>
 			<button onclick={stop}>Stop</button>
@@ -75,10 +131,9 @@
 			</div>
 		</div>
 	</main>
-	<footer>Source on GitHub</footer>
 </div>
 
-<style>
+<style lang="scss">
 	:global(body, html) {
 		width: 100vw;
 		height: 100vh;
@@ -88,14 +143,14 @@
 
 	.container {
 		width: 100%;
-		max-height: 100vh;
+		height: 100vh;
 		display: flex;
 		flex-direction: column;
 	}
 
 	main {
 		width: 80vw;
-		height: 90vh;
+		height: 100%;
 		display: flex;
 		flex-direction: row;
 	}
@@ -105,10 +160,65 @@
 	}
 
 	header {
-		height: 5vh;
+		background-color: #1e1e1e;
+		opacity: 0.9;
+		padding: 0.75rem 0.5rem;
+		display: flex;
+		justify-content: space-between;
+
+
+		.left {
+			display: flex;
+			align-items: center;
+		}
 	}
 
-	footer {
-		height: 5vh;
+	h1 {
+		margin: 0;
+		color: #d4d4d4;
+		font-weight: 800;
+		margin-left: 0.5rem;
+	}
+
+	.editor {
+		width: 100%;
+		padding-top: 1rem;
+		background-color: #1e1e1e;
+		height: calc(100vh - (68px - 1rem) - 2 * 0.75rem - 1rem);
+	}
+
+	.editor:has(> .vimMode) {
+		height: calc(100vh - (68px - 1rem) - 2 * 0.75rem - 1rem - 1rem);
+	}
+
+	.vimMode {
+		background-color: #1e1e1e;
+		color: #d4d4d4;
+		width: 100%;
+	}
+
+	.icon {
+		width: calc(68px - 1rem);
+		height: calc(68px - 1rem);
+	}
+
+	button.smallIcon {
+		width: calc(2 * (68px - 1rem) / 3);
+		height: calc(2 * (68px - 1rem) / 3);
+		background-color: rgba(0, 0, 0, 0);
+		border: none;
+		padding: 0;
+		cursor: pointer;
+	}
+
+	.iconAlign {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.right {
+		display: flex;
+		gap: 0.25rem;
 	}
 </style>
